feb5931eb119bd5b93d5aea2e3b99492eeb40816
Anatoliy Turanskiy <anatol911@yandex.ru>
2024-05-21 22:23:33 +0500

    Добавлен метод, который сортирует символы массива, выбирая три или менее элементов

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿string[] SeparationString(string symbolFromConsole)
+﻿using System.Diagnostics.Metrics;
+
+string[] SeparationString(string symbolFromConsole)
 {
     string[] SymbolArray = new string[symbolFromConsole.Length];
     SymbolArray = symbolFromConsole.Split(",");
     return SymbolArray
+}
+
+string[] HaveThreeSymbolArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0, i < array.Length; i++)
+    {
+        if (array [i].Length <= 3)
+        {
+            count ++;
+        }
+    }
 }
 
 Console.Write("Введите символы, разделенные запятой: "); 
